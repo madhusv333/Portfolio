ec797fc07b35273466e2291892e116ed1724b28f
MADHU S V <73734803+madhusv333@users.noreply.github.com>
2024-07-22 00:58:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log('Resume downloaded');
     });
 });
+
+document.getElementById('mode-toggle').addEventListener('click', function () {
+    document.body.classList.toggle('dark-mode');
+    this.textContent = document.body.classList.contains('dark-mode') ? 'Light Mode' : 'Dark Mode';
+});
